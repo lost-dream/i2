46fe65bc384902535a1082d69f7070ec6c525c90
lvfei <lvfei@qq.com>
2019-07-24 14:10:47 +0800

    'aaa'

---
+++ src/views/modules/i2/Sidefun.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="mod-"></div>
+</template>
+
+<script>
+export default {
+  components: {},
+  props: {},
+  data () {
+    return {
+    }
+  },
+  computed: {},
+  methods: {},
+  created () {},
+  mounted () {}
+}
+</script>
+<style lang="stylus" scoped>
+
+</style>
