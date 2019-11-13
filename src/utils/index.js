cf5c66fb7d551b504ad47a159cca8d821cdf7917
lost-dream <13623673118@163.com>
2019-11-13 10:05:06 +0800

    bug 修改

--- src/utils/index.js
+++ src/utils/index.js
@@ -29,3 +29,34 @@ export function flatten(arr) {
   }
   return res
 }
+
+// 16进制颜色转rgb hex2RGB('#ace') => rgb(170,204,238)
+export function hex2RGB(hex) {
+  const rgb = []
+  hex = hex.substr(1)
+
+  if (hex.length === 3) {
+    hex = hex.replace(/(.)/g, '$1$1')
+  }
+
+  hex.replace(/../g, function(color) {
+    rgb.push(parseInt(color, 0x10))
+  })
+
+  return `rgb(${rgb.join(',')})`
+}
+
+// 16进制颜色转rgb hex2RGB('rgb(170,204,238)') => '#aaccee'
+export function RGB2Hex(rgb) {
+  const color = rgb.toString().match(/\d+/g)
+  let hex = '#'
+
+  for (let i = 0; i < 3; i++) {
+    // 'Number.toString(16)' 是JS默认能实现转换成16进制数的方法.
+    // 'color[i]' 是数组，要转换成字符串.
+    // 如果结果是一位数，就在前面补零。例如： A变成0A
+    hex += ('0' + Number(color[i]).toString(16)).slice(-2)
+  }
+
+  return hex
+}
